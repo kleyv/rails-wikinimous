61d5a6de2c120b9453cf752d4b6cfc683ae04c12
Eukleyv Cardoso <eukleyvcardoso@gmail.com>
2020-08-18 15:11:09 +0000

    generate seed with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |article|
+  article = Article.new(
+    title: Faker::Name.name,
+    content: Faker::Quote.famous_last_words
+    )
+  article.save
+end
